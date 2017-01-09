7315ea8724e82b11eae359681e59f76a0fd26e98
weoz <weoz@ukr.net>
2017-01-09 20:44:47 +0200

    created javaFX variables and refactored GUI

--- src/main/java/ua/kruart/parser/controller/MainController.java
+++ src/main/java/ua/kruart/parser/controller/MainController.java
@@ -1,7 +1,34 @@
 package ua.kruart.parser.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 /**
- * Created by Arthur on 09.01.2017.
+ * Main Controller of the GUI form.
  */
 public class MainController {
+    @FXML
+    public TextField txtAttrColumn;
+    @FXML
+    public TextField txtLinksColumn;
+    @FXML
+    public Button btnChoosingExcel;
+    @FXML
+    public Button btnChoosingDirectory;
+    @FXML
+    public Button btnParsing;
+    @FXML
+    public Label labelPathToFile;
+    @FXML
+    public Label labelPathToDirectory;
+    @FXML
+    public Label labelStatus;
+
+
+    public void actionButtonPressed(ActionEvent actionEvent) {
+
+    }
 }
