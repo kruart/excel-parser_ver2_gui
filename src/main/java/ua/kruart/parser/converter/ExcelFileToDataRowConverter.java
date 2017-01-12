2858922b951e2e363693d21d4c79c905319ac6de
weoz <weoz@ukr.net>
2017-01-12 17:10:43 +0200

    added support files with .xlsx extension

--- src/main/java/ua/kruart/parser/converter/ExcelFileToDataRowConverter.java
+++ src/main/java/ua/kruart/parser/converter/ExcelFileToDataRowConverter.java
@@ -1,10 +1,11 @@
 package ua.kruart.parser.converter;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ua.kruart.parser.model.DataRow;
@@ -27,8 +28,16 @@ public class ExcelFileToDataRowConverter {
     private final static Logger LOGGER = LoggerFactory.getLogger(ExcelFileToDataRowConverter.class);
 
     public static List<DataRow> extractDataFromFile(File src, int attributeColumn, int imageColumn) {
-        try(HSSFWorkbook wb = new HSSFWorkbook(new FileInputStream(src))) { //Создаем workbook
-            HSSFSheet sheet = wb.getSheetAt(0);// Проверяем только первую страницу
+        Workbook wb = null;
+        try {
+            //Создаем workbook
+            if (src.getAbsolutePath().endsWith(".xls")) {
+                wb = new HSSFWorkbook(new FileInputStream(src));    //if file has .xls extension
+            } else {
+                wb = new XSSFWorkbook(new FileInputStream(src));    //if file has .xlsx extension
+            }
+
+            Sheet sheet = wb.getSheetAt(0);// Проверяем только первую страницу
 
             Iterator<Row> rows = sheet.rowIterator(); // gets iterator of the rows
             List<DataRow> dataRowList = new ArrayList<>();
@@ -37,7 +46,7 @@ public class ExcelFileToDataRowConverter {
                 rows.next();
 
             while (rows.hasNext()) { // Перебираем все строки начиная со второй до тех пор, пока документ не закончится
-                HSSFRow row = (HSSFRow) rows.next();
+                Row row = rows.next();
                 DataRow dataRow = extractCellsFromRow(attributeColumn, imageColumn, row);
                 if (dataRow != null) {
                     dataRowList.add(dataRow);
@@ -47,14 +56,22 @@ public class ExcelFileToDataRowConverter {
 
         } catch (IOException e) {
             LOGGER.error("Something has gone wrong. Probably the file cannot be found. Absolute path to file: {}. Please select the file again.", src.getAbsoluteFile());
+        } finally {
+            if (wb != null) {
+                try {
+                    wb.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return new ArrayList<>();
     }
 
-    private static DataRow extractCellsFromRow(int attributeColumn, int imageColumn, HSSFRow row) {
+    private static DataRow extractCellsFromRow(int attributeColumn, int imageColumn, Row row) {
         //Получаем ячейки из строки по номерам столбцов
-        HSSFCell attributeCell = row.getCell(attributeColumn);
-        HSSFCell imageLinkCell = row.getCell(imageColumn);
+        Cell attributeCell = row.getCell(attributeColumn);
+        Cell imageLinkCell = row.getCell(imageColumn);
         DataRow dataRow = null;
 
         if (isCellValid(attributeCell) && isCellValid(imageLinkCell)) {
@@ -67,7 +84,7 @@ public class ExcelFileToDataRowConverter {
     /**
      * Checks that HSSFCell object isn't null and not empty
      */
-    private static boolean isCellValid(HSSFCell cell) {
+    private static boolean isCellValid(Cell cell) {
         return cell != null && !cell.getStringCellValue().isEmpty();
     }
 
